d8a2b3fa122b532df8bc723a38813745b6d45978
Owcollier <owcollier@gmail.com>
2018-08-27 16:51:34 -0400

    added container class div to wrap footer content, added background color in css

--- index.php
+++ index.php
@@ -99,7 +99,11 @@
     </div><!-- /.container -->
 
     <footer class="footer">
-      <span>const sayHelloBot = (encouragement) => { return (encouragement ? '&ltspan&gtHello world&lt/span&gt' : '&ltspan&gt...&lt/span&gt') };</span>
+
+      <div class="container">
+        <span>const sayHelloBot = (encouragement) => { return (encouragement ? '&ltspan&gtHello world&lt/span&gt' : '&ltspan&gt...&lt/span&gt') };</span>
+      <div>
+        
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
