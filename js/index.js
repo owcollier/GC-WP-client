a7f07812254b5b023c9480b9e45762b8a98fcfd4
Owcollier <owcollier@gmail.com>
2018-08-27 17:35:45 -0400

    capturing footer button click

--- js/index.js
+++ js/index.js
@@ -4,11 +4,16 @@ const dingDong = (event) => {
   console.log('DONG!');
 };
 
+const logEmail = (event) => {
+  console.log('heyo');
+}
+
 (function($) {
 	
 	// Wrapper function for intializing jQuery $
   console.log('hello world');
 
   $(document).on('click', '.blog-post-dinger', dingDong);
+  $(document).on('click', '.footer-button', logEmail);
 	
 })( jQuery );
