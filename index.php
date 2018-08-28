5d4b649c8580a59301df4e83d27e89b617f584c6
Owcollier <owcollier@gmail.com>
2018-08-28 13:22:08 -0400

    added container div for content in main content column posts with requisite class

--- index.php
+++ index.php
@@ -66,108 +66,124 @@
         <div class="col-7 content-main">
           <div class="content-main-post">
             <h2 class="content-main-title">TEACHING & LEARNING ENVIRONMENT</h2>
-            <ul class="ul-primary">
-              <li>Small and large group settings</li>
-              <li>Class size: 15</li>
-              <li>Courses in the classroom, online, and in the community</li>
-              <li>Individual, team projects, and independent study</li>
-              <li>Labs and Meet-Ups</li>
-              <li>One-on-one support</li>
-              <li>Rigorous yet nurturing academic environment</li>
-              <li>Safe to take intellectual risks</li>
-            </ul>
+            <div class="content-main-container">
+              <ul class="ul-primary">
+                <li>Small and large group settings</li>
+                <li>Class size: 15</li>
+                <li>Courses in the classroom, online, and in the community</li>
+                <li>Individual, team projects, and independent study</li>
+                <li>Labs and Meet-Ups</li>
+                <li>One-on-one support</li>
+                <li>Rigorous yet nurturing academic environment</li>
+                <li>Safe to take intellectual risks</li>
+              </ul>
+            </div>
           </div>
 
           <div class="content-main-post">
             <h2 class="content-main-title">CURRICULUM</h2>
-            <ul class="ul-primary">
-              <li>21 credits for middle school; 30 credits for high school</li>
-              <li>Dual Enrollment and Advanced Placement Courses</li>
-              <li>Instructors: certified teachers, business and music industry reps, and college professors</li>
-              <li>Select one of four majors:
-                <ul class="ul-secondary">
-                  <li>Music Production & Entrepreneurship</li>
-                  <li>Game Innovation Design & Entrepreneurship</li>
-                  <li>Business of Communication</li>
-                  <li>Business of Science</li>
-                <ul>
-              </li>
-            <ul>
+            <div class="content-main-container">
+              <ul class="ul-primary">
+                <li>21 credits for middle school; 30 credits for high school</li>
+                <li>Dual Enrollment and Advanced Placement Courses</li>
+                <li>Instructors: certified teachers, business and music industry reps, and college professors</li>
+                <li>Select one of four majors:
+                  <ul class="ul-secondary">
+                    <li>Music Production & Entrepreneurship</li>
+                    <li>Game Innovation Design & Entrepreneurship</li>
+                    <li>Business of Communication</li>
+                    <li>Business of Science</li>
+                  <ul>
+                </li>
+              <ul>
+            </div>
           </div>
 
           <div class="content-main-post">
             <h2 class="content-main-title">GRADUATION MODEL</h2>
-            <ul class="ul-primary">
-              <li>Total credits reflect a high profile with universities</li>
-              <li>3 graduation options:
-                <ul class="ul-secondary">
-                  <li>3 years with a high school diploma</li>
-                  <li>4 years with a high school diploma and an AA Degree</li>
-                  <li>5 years with a high school diploma</li>
-                </ul>
-              </li>
-              <li>Diplomas: Superlative, Exemplary, and Distinction</li>
-              <li>Models exceed State of Florida requirements</li>
-            </ul>
+            <div class="content-main-container">
+              <ul class="ul-primary">
+                <li>Total credits reflect a high profile with universities</li>
+                <li>3 graduation options:
+                  <ul class="ul-secondary">
+                    <li>3 years with a high school diploma</li>
+                    <li>4 years with a high school diploma and an AA Degree</li>
+                    <li>5 years with a high school diploma</li>
+                  </ul>
+                </li>
+                <li>Diplomas: Superlative, Exemplary, and Distinction</li>
+                <li>Models exceed State of Florida requirements</li>
+              </ul>
+            </div>
           </div>
 
           <div class="content-main-post">
             <h2 class="content-main-title">COUNSELING CENTER</h2>
-            <ul class="ul-primary">
-              <li>Academic, college and career counselers</li>
-              <li>Success Coach</li>
-              <li>Psychologist and Rap Room</li>
-              <li>School Resource Officer</li>
-            </ul>
+            <div class="content-main-container">
+              <ul class="ul-primary">
+                <li>Academic, college and career counselers</li>
+                <li>Success Coach</li>
+                <li>Psychologist and Rap Room</li>
+                <li>School Resource Officer</li>
+              </ul>
+            </div>
           </div>
 
           <div class="content-main-post">
             <h2 class="content-main-title">STUDENT LIFE</h2>
-            <ul class="ul-primary">
-              <li>Student Government</li>
-              <li>Honor Societies</li>
-              <li>Honor Court</li>
-              <li>Red Carpet Focus Group with Principal</li>
-              <li>School Strategic Planning Team</li>
-              <li>Student-run Jazz Club</li>
-              <li>Hobby Clubs</li>
-              <li>Athletics</li>
-            </ul>
+            <div class="content-main-container">
+              <ul class="ul-primary">
+                <li>Student Government</li>
+                <li>Honor Societies</li>
+                <li>Honor Court</li>
+                <li>Red Carpet Focus Group with Principal</li>
+                <li>School Strategic Planning Team</li>
+                <li>Student-run Jazz Club</li>
+                <li>Hobby Clubs</li>
+                <li>Athletics</li>
+              </ul>
+            </div>
           </div>
 
           <div class="content-main-post">
             <h2 class="content-main-title">ADMISSION PROCESS & CRITERIA</h2>
-            <ul class="ul-primary">
-              <li>Apply online</li>
-              <li>Review process by staff</li>
-              <li>Interview scheduled</li>
-              <li>Audition or portfolio presentation</li>
-              <li>Academic, Attendance, and Conduct Reports</li>
-              <li>References</li>
-              <li>Hobbies, Service, and Interests</li>
-              <li>SSAT</li>
-            </ul>
+            <div class="content-main-container">
+              <ul class="ul-primary">
+                <li>Apply online</li>
+                <li>Review process by staff</li>
+                <li>Interview scheduled</li>
+                <li>Audition or portfolio presentation</li>
+                <li>Academic, Attendance, and Conduct Reports</li>
+                <li>References</li>
+                <li>Hobbies, Service, and Interests</li>
+                <li>SSAT</li>
+              </ul>
+            </div> 
           </div>
 
           <div class="content-main-post">
             <h2 class="content-main-title">PARENT & COMMUNITY PARTICIPATION</h2>
-            <ul class="ul-primary">
-              <li>Parent Association</li>
-              <li>Parent Center: Workshops, Speakers, Events, Strategic Planning Team with head of school</li>
-              <li>Community partnerships with corporations, government, business, associations, organizations, and foundations</li>
-              <li>Mentors, Motivators, and Role Models</li>
-            </ul>
+            <div class="content-main-container">
+              <ul class="ul-primary">
+                <li>Parent Association</li>
+                <li>Parent Center: Workshops, Speakers, Events, Strategic Planning Team with head of school</li>
+                <li>Community partnerships with corporations, government, business, associations, organizations, and foundations</li>
+                <li>Mentors, Motivators, and Role Models</li>
+              </ul>
+            </div>
           </div>
 
           <div class="content-main-post">
             <h2 class="content-main-title">MUSIC & ENTERTAINMENT EDUCATION FUND</h2>
             <span class="content-main-subtitle">(Community Partner)</span>
-            <ul class="ul-primary">
-              <li>Merit and need based scholarships available</li>
-              <li>Corporate partnership employee plan available</li>
-              <li>Staff discounts available</li>
-              <li>Sibling discounts available</li>
-            </ul>
+            <div class="content-main-container">
+              <ul class="ul-primary">
+                <li>Merit and need based scholarships available</li>
+                <li>Corporate partnership employee plan available</li>
+                <li>Staff discounts available</li>
+                <li>Sibling discounts available</li>
+              </ul>
+            </div>
           </div>
 
       </div>
