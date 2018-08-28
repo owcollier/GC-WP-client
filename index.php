0eb3dd546cff8c98fecf9392faf74718e8ec6917
Owcollier <owcollier@gmail.com>
2018-08-28 12:57:27 -0400

    building out more of the html structure for launch content

--- index.php
+++ index.php
@@ -69,28 +69,70 @@
 
           <div class="content-main-post">
             <h2 class="content-main-title">TEACHING & LEARNING ENVIRONMENT</h2>
-
-            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-            <blockquote>
-              <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            </blockquote>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+            <ul class="ul-primary">
+              <li>Small and large group settings</li>
+              <li>Class size: 15</li>
+              <li>Courses in the classroom, online, and in the community</li>
+              <li>Individual, team projects, and independent study</li>
+              <li>Labs and Meet-Ups</li>
+              <li>One-on-one support</li>
+              <li>Rigorous yet nurturing academic environment</li>
+              <li>Safe to take intellectual risks</li>
+            </ul>
           </div>
 
           <div class="content-main-post">
             <h2 class="content-main-title">CURRICULUM</h2>
-
-            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <ul class="ul-primary">
+              <li>21 credits for middle school; 30 credits for high school</li>
+              <li>Dual Enrollment and Advanced Placement Courses</li>
+              <li>Instructors: certified teachers, business and music industry reps, and college professors</li>
+              <li>Select one of four majors:
+                <ul class="ul-secondary">
+                  <li>Music Production & Entrepreneurship</li>
+                  <li>Game Innovation Design & Entrepreneurship</li>
+                  <li>Business of Communication</li>
+                  <li>Business of Science</li>
+                <ul>
+              </li>
             <ul>
-              <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-              <li>Donec id elit non mi porta gravida at eget metus.</li>
-              <li>Nulla vitae elit libero, a pharetra augue.</li>
-            </ul>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
           </div>
-        </div>
+
+          <div class="content-main-post">
+            <h2 class="content-main-title"></h2>
+            <ul class="ul-primary">
+            </ul>
+          </div>
+
+          <div class="content-main-post">
+            <h2 class="content-main-title"></h2>
+            <ul class="ul-primary">
+            </ul>
+          </div>
+
+          <div class="content-main-post">
+            <h2 class="content-main-title"></h2>
+            <ul class="ul-primary">
+            </ul>
+          </div>
+
+          <div class="content-main-post">
+            <h2 class="content-main-title"></h2>
+            <ul class="ul-primary">
+            </ul>
+          </div>
+
+          <div class="content-main-post">
+            <h2 class="content-main-title"></h2>
+            <ul class="ul-primary">
+            </ul>
+          </div>
+
+          <div class="content-main-post">
+            <h2 class="content-main-title"></h2>
+            <ul class="ul-primary">
+            </ul>
+          </div>
 
       </div>
 
