dc1349c4c88702ce0e49b6d90776d3a3f4ac0545
Owcollier <owcollier@gmail.com>
2018-08-28 20:02:50 -0400

    refactored jQuery scroll listener, created scrollSidebar function for sticky sidebar logic now passed along as callback to scroll listener

--- js/index.js
+++ js/index.js
@@ -11,69 +11,40 @@ const logEmail = (event) => {
   console.log(email);
 };
 
+const scrollSidebar = () => {
+  const length = $('#left').height() - $('#sidebar').height() + $('#left').offset().top;
+  const scroll = $(this).scrollTop();
+  const height = $('#sidebar').height() + 'px';
+
+  if (scroll < $('#left').offset().top - 120) {
+
+    $('#sidebar').css({
+      'position': 'absolute',
+      'top': '0'
+    });
+
+  } else if (scroll > length) {
+
+    $('#sidebar').css({
+      'position': 'absolute',
+      'bottom': '0',
+      'top': 'auto'
+    });
+
+  } else {
+
+    $('#sidebar').css({
+      'position': 'fixed',
+      'top': '120px',
+      'height': height
+    });
+  }
+};
+
 (function($) {
 
-  // const length = $('#left').height() - $('#sidebar').height();
+  $(window).on('scroll', scrollSidebar);
 
-  const length = $('#left').height() - $('#sidebar').height() + $('#left').offset().top;
-
-  $(window).scroll(function () {
-
-    const scroll = $(this).scrollTop();
-    const height = $('#sidebar').height() + 'px';
-
-    if (scroll < $('#left').offset().top - 120) {
-
-      $('#sidebar').css({
-        'position': 'absolute',
-        'top': '0'
-      });
-
-    } else if (scroll > length) {
-
-      $('#sidebar').css({
-        'position': 'absolute',
-        'bottom': '0',
-        'top': 'auto'
-      });
-
-    } else {
-
-      $('#sidebar').css({
-        'position': 'fixed',
-        'top': '120px',
-        'height': height
-      });
-    }
-  });
-
-  // $(window).scroll(function () {
-  //   const scroll = $(this).scrollTop();
-  //   const height = $('#sidebar').height() + 'px';
-
-  //   if(scroll <= 0) {
-  //     $('#sidebar').css({
-  //       'position': 'absolute',
-  //       'top': '0'
-  //     });
-  //   } else if(scroll >= length) {
-  //     $('#sidebar').css({
-  //       'position': 'absolute',
-  //       'bottom': '0',
-  //       'top': 'auto'
-  //     });
-  //   } else {
-  //     $('#sidebar').css({
-  //       'position': 'fixed',
-  //       'top': '300px'
-  //     });
-  //   }
-  // });
-	
-  // Wrapper function for intializing jQuery $
-  console.log('hello world');
-
-  // $(document).on('click', '.blog-post-dinger', dingDong);
   $(document).on('click', '.footer-button', logEmail);
 	
 })( jQuery );
