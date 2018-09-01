41aa4379d8c8ff9e411e037866b50c140023efac
Owcollier <owcollier@gmail.com>
2018-08-31 21:35:17 -0400

    removed artifact comments of previous attempt at sidebar section highlight logic

--- js/index.js
+++ js/index.js
@@ -68,6 +68,7 @@ const handleSidebarNavigation = (event) => {
 };
 
 const handleSidebarHighlight = () => {
+
   const scroll =$(this).scrollTop();
   const scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
 
@@ -82,105 +83,52 @@ const handleSidebarHighlight = () => {
     contentBlockEight: $('#contentBlockEight').offset().top - $('#contentBlockOne').height() - $('#masthead').height()
   };
 
-  // let currentFocus = {
-  //   focus: ''
-  // };
-
   if (scroll > scrollPositions.contentBlockOne && scroll < scrollPositions.contentBlockTwo) {
-
-    // if (currentFocus.focus !== '#sidebarOne') {
-    //   $(currentFocus.focus).removeClass('sidebar-focus');
-    //   currentFocus.focus = '#sidebarOne';
-    // }
 
     $('.content-sidebar-li').removeClass('sidebar-focus');
     $('#sidebarOne').addClass('sidebar-focus');
 
   } else if (scroll > scrollPositions.contentBlockTwo && scroll < scrollPositions.contentBlockThree) {
 
-    // if (currentFocus.focus !== '#sidebarTwo') {
-    //   $(currentFocus.focus).removeClass('sidebar-focus');
-    //   currentFocus.focus = '#sidebarTwo';
-    // }
-
     $('.content-sidebar-li').removeClass('sidebar-focus');
     $('#sidebarTwo').addClass('sidebar-focus');
 
   } else if (scroll > scrollPositions.contentBlockThree && scroll < scrollPositions.contentBlockFour) {
-
-    // if (currentFocus !== '#sidebarThree') {
-    //   $(currentFocus).removeClass('sidebar-focus');
-    //   currentFocus = '#sidebarThree';
-    // }
 
     $('.content-sidebar-li').removeClass('sidebar-focus');
     $('#sidebarThree').addClass('sidebar-focus');
 
   } else if (scroll > scrollPositions.contentBlockFour && scroll < scrollPositions.contentBlockFive) {
 
-    // if (currentFocus !== '#sidebarFour') {
-    //   $(currentFocus).removeClass('sidebar-focus');
-    //   currentFocus = '#sidebarFour';
-    // }
-
     $('.content-sidebar-li').removeClass('sidebar-focus');
     $('#sidebarFour').addClass('sidebar-focus');
 
   } else if (scroll > scrollPositions.contentBlockFive && scroll < scrollPositions.contentBlockSix) {
-
-    // if (currentFocus !== '#sidebarFive') {
-    //   $(currentFocus).removeClass('sidebar-focus');
-    //   currentFocus = '#sidebarFive';
-    // }
 
     $('.content-sidebar-li').removeClass('sidebar-focus');
     $('#sidebarFive').addClass('sidebar-focus');
 
   } else if (scroll > scrollPositions.contentBlockSix && scroll < scrollPositions.contentBlockSeven) {
 
-    // if (currentFocus !== '#sidebarSix') {
-    //   $(currentFocus).removeClass('sidebar-focus');
-    //   currentFocus = '#sidebarSix';
-    // }
-
     $('.content-sidebar-li').removeClass('sidebar-focus');
     $('#sidebarSix').addClass('sidebar-focus');
 
   } else if (scroll > scrollPositions.contentBlockSeven && scroll < scrollPositions.contentBlockEight) {
-
-    // if (currentFocus !== '#sidebarSeven') {
-    //   $(currentFocus).removeClass('sidebar-focus');
-    //   currentFocus = '#sidebarSeven';
-    // }
 
     $('.content-sidebar-li').removeClass('sidebar-focus');
     $('#sidebarSeven').addClass('sidebar-focus');
 
   } else if (scroll > scrollPositions.contentBlockEight && scrollBottom !== 0) {
 
-    // if (currentFocus !== '#sidebarEight') {
-    //   $(currentFocus).removeClass('sidebar-focus');
-    //   currentFocus = '#sidebarEight';
-    // }
-
     $('.content-sidebar-li').removeClass('sidebar-focus');
     $('#sidebarEight').addClass('sidebar-focus');
 
   } else if (scrollBottom === 0) {
 
-    // if (currentFocus !== '#sidebarContact') {
-    //   $(currentFocus).removeClass('sidebar-focus');
-    //   currentFocus = '#sidebarContact';
-    // }
-
     $('.content-sidebar-li').removeClass('sidebar-focus');
     $('#sidebarContact').addClass('sidebar-focus');
 
   }
-  
-  // else if (scroll + $(window).height() / $(document).height() === 0) {
-  //   console.log('Contact!');
-  // }
 
 };
 
