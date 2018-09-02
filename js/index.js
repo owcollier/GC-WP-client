ce5565896b2b52f7a019428c3066d7c24d97d699
Owcollier <owcollier@gmail.com>
2018-09-02 19:22:19 -0400

    testing touchstart for jquery on click functionality for dropdown navigation links on mobile

--- js/index.js
+++ js/index.js
@@ -305,6 +305,6 @@ const handleDropdownNavigation = (event) => {
 
   $(document).on('click', '.navbar-toggler', handleDropdown);
   $(document).on('click', '.sidebar-link', handleSidebarNavigation);
-  $(document).on('click touch', '.dropdown-link', handleDropdownNavigation);
+  $(document).on('touchstart click', '.dropdown-link', handleDropdownNavigation);
 	
 })( jQuery );
