aa10c3e37ea02dc74193e4b7ca5bd5172165cd57
Owcollier <owcollier@gmail.com>
2018-09-04 10:33:07 -0400

    using new cdn for bootstrap library, added fallback for custom web font on header tags

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <title>Gables Crest</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
 
     <!-- Custom styles for this template -->
     <link href="<?php echo get_bloginfo('template_directory'); ?>/main.css" rel="stylesheet">
@@ -313,7 +313,7 @@
     </footer>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <script src="<?php echo get_bloginfo('template_directory'); ?>/js/index.js"></script>
   </body>
 </html>
